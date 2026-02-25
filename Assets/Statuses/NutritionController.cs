bbfa63c25766109ba730653bfd70b953c7c10230
toblerus <37512320+toblerus@users.noreply.github.com>
2026-02-26 00:04:13 +0100

    Adds hunger and thirst and reactive timer

---
+++ Assets/Statuses/NutritionController.cs
@@ -0,0 +1,7 @@
+﻿namespace Statuses
+{
+    public class NutritionController
+    {
+        
+    }
+}
---
+++ Assets/Statuses/NutritionModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveCore;
+using Saving;
+
+namespace Statuses
+{
+    public class NutritionModel
+    {
+        public ReactiveValue<int> Hunger { get; set; } = new(100);
+        public ReactiveValue<int> Thirst { get; set; } = new(100);
+
+        public NutritionModel()
+        {
+            if (ES3.KeyExists(SavegameConstants.HungerStatus))
+            {
+                Hunger.Value = ES3.Load<int>(SavegameConstants.HungerStatus);
+            }
+            
+            if (ES3.KeyExists(SavegameConstants.ThirstStatus))
+            {
+                Hunger.Value = ES3.Load<int>(SavegameConstants.ThirstStatus);
+            }
+            
+            Hunger.Subscribe(value => ES3.Save(SavegameConstants.HungerStatus, value));
+            Thirst.Subscribe(value => ES3.Save(SavegameConstants.ThirstStatus, value));
+        }
+    }
+}
